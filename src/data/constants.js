db9a2fd34af36b123d607694f88a579cad405503
Vishnukchandran <vishnuvkc@gmail.com>
2024-07-17 22:24:55 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,156 @@
+export const Bio = {
+  name: "Vishnu",
+  roles: ["TECHNICAL SUPPORT ENGINEER", "REACTJS DEVELOPER"],
+  description:
+    "Hi, I'm Vishnu – a passionate frontend developer specializing in React. I create beautiful and user-friendly websites with a strong focus on functionality and aesthetics. My enthusiasm for web development drives me to continuously learn and expand my skillset. I thrive on challenges and am always eager to explore new technologies.I consider myself a result-oriented individual who excels both as a team player and as an independent worker. I'm diligent, easygoing, and have a good sense of humor, which helps me maintain a positive and productive work environment. I'm excited about the opportunity to bring my skills and enthusiasm to your team and contribute to creating exceptional web experiences",
+  github: "https://github.com/Vishnukchandran",
+  resume: "",
+  linkedin: "",
+  email: "vishnuvkc@gmail.com",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://i.pinimg.com/474x/66/8f/03/668f031e4dde7816c77d8e36b080a94f.jpg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://i.pinimg.com/474x/82/a0/d0/82a0d0900cb1a2801b2a0f5988991baf.jpg",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    role: "Techinal Support Engineer",
+    company: "Remo Software Private Limited",
+    date: "Feb 2018 - Sep 2023",
+    desc: "As an Techinal Support Engineer at Remo Software Private Limited Company, I have gained valuable hands-on experience handling customer queries as a Customer Technical Support Engineer. I am dedicated to maintaining high delivery standards, optimizing processes, and collaborating cross-functionally for efficient problem resolution. My skills include quality management, effective communication, and data analysis. I specialize in ensuring top-tier service during the last mile of the delivery process, consistently meeting and exceeding customer expectations.",
+    skills: ["MS Office", "Fresh desk", "other software tools at the company"],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    school: "KG College of Arts and Science, Coimbatore",
+    date: "June 2013 - May 2016",
+    grade: "First class",
+    degree: "Bachelor of Computer Applications (BCA)",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: " Calculator",
+    date: "Mar 2024",
+    description:
+      "A web application designed to help you calculate simple calculation",
+    image:
+      "",
+    tags: ["HTML", "CSS", "JAVASCRIPT"],
+    category: "web app",
+    github:
+      "",
+    webapp: "",
+    member: [
+      {
+        name: "Vishnu KC",
+        linkedin: "",
+        github: "https://github.com/Vishnukchandran",
+      },
+    ],
+  },
+  // {
+  //   id: 1,
+  //   title: "Todo-App",
+  //   date: "Feb 2024",
+  //   description:
+  //     "Introducing my latest project: a dynamic Todo app featuring robust CRUD operations. Seamlessly manage tasks with create, read, update, and delete functionality. Experience streamlined productivity in a sleek and intuitive interface.",
+  //   image:
+  //     "https://github.com/HarikaranSubramanian/MyTodoApp/blob/main/Screenshot%202024-04-11%20223718.png?raw=true",
+  //   tags: ["React Js"],
+  //   category: "web app",
+  //   github: "https://github.com/HarikaranSubramanian/MyTodoApp.git",
+  //   webapp: "https://mytodo-netlify.netlify.app/",
+  // },
+  // {
+  //   id: 2,
+  //   title: "Book Data Management System",
+  //   date: "Feb 2024 - Present",
+  //   description:
+  //     "This is a simple CRUD application built with HTML, CSS, JavaScript, and React for managing book data. It allows users to perform Create, Read, Update, and Delete (CRUD) operations on book records. The application uses Formik for form validation and integrates with a backend API for data storage and retrieval.",
+  //   image:
+  //     "https://github.com/HarikaranSubramanian/React-Formik-Task/blob/main/Screenshot%202024-04-11%20224950.png?raw=true",
+  //   tags: ["React Js", "Mockapi"],
+  //   category: "web app",
+  //   github: "https://github.com/HarikaranSubramanian/React-Formik-Task.git",
+  //   webapp: "https://netlify-formik.netlify.app/home",
+  // },
+];
+
+export const TimeLineData = [
+  { year: 2022, text: "Started my journey" },
+  { year: 2023, text: "Shared my projects with the world" },
+];
